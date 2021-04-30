c85ffd43378def759c2955ed7fde5f4ec4210cbc
Caleb Zulawski <caleb.zulawski@gmail.com>
2021-04-30 00:44:26 +0000

    Update brew tap

--- symtool.rb
+++ symtool.rb
@@ -1,14 +1,14 @@
 class Symtool < Formula
-  version '0.1.2'
+  version '0.1.4'
   desc "Manipulate static symbols in ELF and Mach-O objects"
   homepage "https://github.com/calebzulawski/symtool"
 
   if OS.mac?
     url "https://github.com/calebzulawski/symtool/releases/download/#{version}/symtool-macos-x86_64.tar.gz"
-    sha256 "3ae85c3a613d19f52f879821b43941e8daa2c01dfad98fd709c045bb5bf7c187"
+    sha256 "c08ee3b50eff66e1dd536e7cdf28595f8270015ae206049a5733dca102dc9b57"
   elsif OS.linux?
     url "https://github.com/calebzulawski/symtool/releases/download/#{version}/symtool-linux-x86_64.tar.gz"
-    sha256 "f0ea98b2e99fb4694aaedf63d680d26923b0a625862e84348115a69c34790e6f"
+    sha256 "6e85befff3aefd186313d3ad5a6a47600fe473b6772b07ea0f34464c23343035"
   end
 
   def install
